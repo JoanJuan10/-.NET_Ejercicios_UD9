ac4e6a5a5825d1833e0bc7f4bcdc6af5e5075c30
JoanJuan10 <iijoanjuan10@gmail.com>
2020-12-31 10:11:13 +0100

    EJERCICIO 1

---
+++ Ejercicios/Ejercicios/EJ1/Alumno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicios.EJ1
+{
+    class Alumno
+    {
+        private string nombre;
+        private int edad;
+
+        public Alumno(string nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Edad { get => edad; set => edad = value; }
+
+        public bool MayorEdad()
+        {
+            if (this.edad >= 18)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Ejercicios/Ejercicios/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ejercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Que ejercicio quieres ejecutar?");
+
+            Console.WriteLine("1 = Ejercicio 1");
+            Console.WriteLine("2 = Ejercicio 2");
+            Console.WriteLine("3 = Ejercicio 3");
+            Console.WriteLine("4 = Ejercicio 4");
+            Console.WriteLine("5 = Ejercicio 5");
+            Console.WriteLine("6 = Ejercicio 6");
+            Console.WriteLine("7 = Ejercicio 7");
+            Console.WriteLine("8 = Ejercicio 8");
+            Console.WriteLine("9 = Ejercicio 9");
+            Console.WriteLine("10 = Ejercicio 10");
+
+
+            string ejercicio = Console.ReadLine();
+
+            switch (ejercicio)
+            {
+                case "1":
+                    EJ1.Ejercicio1 ej1 = new EJ1.Ejercicio1();
+                    ej1.Ejercicio();
+                    break;
+            }
+        }
+    }
+}
